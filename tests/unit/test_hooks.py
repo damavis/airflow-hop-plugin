da2c092f53a73d5446b726e86b9fc62044253e41
PereAL7 <codeper128@gmail.com>
2022-07-13 11:12:10 +0200

    Docker compose set up

--- tests/unit/test_hooks.py
+++ tests/unit/test_hooks.py
@@ -12,3 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from unittest import TestCase
+
+class TestHopHook(TestCase):
+
+    def test_dummy(self):
+        pass
